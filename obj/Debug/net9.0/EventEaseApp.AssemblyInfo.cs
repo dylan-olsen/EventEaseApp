d2b94943b320d52a4da0653a66b068dbaa6b60da
dylan-olsen <166101114+dylan-olsen@users.noreply.github.com>
2025-03-20 14:01:11 +0200

    Improved Bookings GUI

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1268d1c2ef7fb4e6e4692291935a87cb45a70d15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c0fc23aaf35dfe5c882590efba5982e87c2070")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
