00847f4b45001633b0f438ffc8606a9d69332f2a
dylan-olsen <166101114+dylan-olsen@users.noreply.github.com>
2025-04-04 12:39:04 +0200

    Updated Event and Booking views

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b94943b320d52a4da0653a66b068dbaa6b60da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8204e8c8def309f0539a2bd1eb494355173d0874")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
